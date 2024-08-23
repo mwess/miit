7b7976da394b2ae186b223f81bbb6652422cc512
mwess <maximilian.wess@ntnu.no>
2024-08-23 11:28:07 +0200

    added new greedyfhist version. increased version number

--- miit/registerers/greedy_f_hist_ext.py
+++ miit/registerers/greedy_f_hist_ext.py
@@ -1,9 +1,9 @@
 from dataclasses import dataclass
 from typing import Any, ClassVar, Dict, Optional, List, Tuple
 
-import greedyfhist
-import greedyfhist as gfh
+import greedyfhist, greedyfhist as gfh
 from greedyfhist.options import RegistrationOptions
+from greedyfhist.registration.greedy_f_hist import RegistrationResult, GroupwiseRegResult
 import numpy
 
 from .base_registerer import Registerer, RegistrationResult
@@ -11,19 +11,13 @@ from .base_registerer import Registerer, RegistrationResult
 @dataclass
 class GreedyFHistRegistrationResult(RegistrationResult):
 
-    registration_result: greedyfhist.registration.greedy_f_hist.RegistrationResult
+    registration_result: RegistrationResult
 
 
 @dataclass
 class GreedyFHistGroupRegistrationResult(RegistrationResult):
     
-    registration_result: greedyfhist.registration.greedy_f_hist.GroupwiseRegResult
-
-
-@dataclass
-class GreedyFHistGroupwiseRegistrationOptions:
-    pass
-
+    registration_result: GroupwiseRegResult
 
 @dataclass
 class GreedyFHistExt(Registerer):
@@ -37,11 +31,12 @@ class GreedyFHistExt(Registerer):
     def register_images(self, 
                         moving_img: numpy.array, 
                         fixed_img: numpy.array, 
-                        **kwargs: Dict) -> GreedyFHistRegistrationResult:
-        moving_img_mask = kwargs.get('moving_img_mask', None)
-        fixed_img_mask = kwargs.get('fixed_img_mask', None)
-        options = RegistrationOptions()
-        # options.parse_dict(kwargs)
+                        moving_img_mask: Optional[numpy.array] = None,
+                        fixed_img_mask: Optional[numpy.array] = None,
+                        options: Optional[RegistrationOptions] = None
+                        ) -> GreedyFHistRegistrationResult:
+        if options is None:
+            options = RegistrationOptions()
         reg_result = self.registerer.register(moving_img=moving_img,
                                               fixed_img=fixed_img,
                                               moving_img_mask=moving_img_mask,
@@ -54,7 +49,7 @@ class GreedyFHistExt(Registerer):
                            pointset: numpy.array, 
                            transformation: GreedyFHistRegistrationResult, 
                            **kwargs: Dict) -> numpy.array:
-        transformed_pointset = self.registerer.transform_pointset(pointset, transformation.registration_result.backward_transform)
+        transformed_pointset = self.registerer.transform_pointset(pointset, transformation.registration_result.registration_transforms.backward_transform)
         # transformation_result = self.registerer.transform_pointset(pointset, transformation.backward_displacement_field, **kwargs)
         return transformed_pointset 
     
@@ -63,12 +58,8 @@ class GreedyFHistExt(Registerer):
                         transformation: GreedyFHistRegistrationResult, 
                         interpolation_mode: str, 
                         **kwargs: Dict) -> numpy.array:
-        warped_image = self.registerer.transform_image(image, transformation.registration_result.forward_transform, interpolation_mode)
+        warped_image = self.registerer.transform_image(image, transformation.registration_result.registration_transforms.forward_transform, interpolation_mode)
         return warped_image
-
-    # TODO: Remove this.
-    def get_default_args(self):
-        return gfh.registration.get_default_args()
 
     @classmethod
     def load_from_config(cls, config: Optional[Dict[str, Any]] = None):
@@ -79,11 +70,10 @@ class GreedyFHistExt(Registerer):
 
     def groupwise_registration(self,
                                image_with_mask_list: List[Tuple[numpy.array, Optional[numpy.array]]],
-                               skip_deformable_registration=False,
-                               affine_registration_options: Optional[RegistrationOptions] = None,
-                               nonrigid_registration_options: Optional[RegistrationOptions] = None,
-                               **kwargs: Dict) -> Tuple[List[GreedyFHistRegistrationResult], Optional[GreedyFHistGroupRegistrationResult]]:
-        group_reg, _ = self.registerer.groupwise_registration(image_with_mask_list, skip_deformable_registration=skip_deformable_registration)
+                               options: Optional[RegistrationOptions] = None) -> Tuple[List[GreedyFHistRegistrationResult], Optional[GreedyFHistGroupRegistrationResult]]:
+        if options is None:
+            options = RegistrationOptions()
+        group_reg, _ = self.registerer.groupwise_registration(image_with_mask_list, options=options)
         # To make it possible to apply transformations separately, we need to split them up before returning.
         transform_list = []
         for idx in range(len(image_with_mask_list)-1):
